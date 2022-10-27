a97a384727ac8e59aa1c3bc2cd95f25ef920260b
Jiří Zapletal <jz@strategio.digital>
2022-10-27 22:47:24 +0200

    Added execution time in HompeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,17 +8,20 @@ declare(strict_types=1);
 namespace Framework\Controller;
 
 use Framework\Storage\Storage;
+use Nette\DI\Container;
 
 class HomeController extends Controller
 {
-    public function index(Storage $storage): void
+    public function index(Storage $storage, Container $container): void
     {
         $dt = new \DateTime();
+        $executionTime = microtime(true) - $container->parameters['startedAt'];
         
         $this->getResponse()->send([
             'name' => $_ENV['APP_NAME'],
             'mode' => $_ENV['APP_ENV_MODE'],
             'storage_adapter' => $storage->getAdapterName(),
+            'execution_time' => floor($executionTime * 1000) . 'ms',
             'current_dt' => [
                 'date_time' => $dt->format('Y.m.d H:i:s:u'),
                 'time_zone' => $dt->getTimezone()->getName()
