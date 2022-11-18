3235004908c71673e46a6590b45a16a85dce8877
Jiří Zapletal <jz@strategio.digital>
2022-11-18 19:06:51 +0100

    Added project build

---
+++ dist/assets/Dashboard.1e617ad9.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a}from"./index.52efcff5.js";const c={},t=a("h1",null,"Dashboard",-1),n=[t];function r(_,d){return o(),s("div",null,n)}const f=e(c,[["render",r]]);export{f as default};
---
+++ dist/assets/strategio.04befae5.js
@@ -0,0 +1,1 @@
+const s="/assets/strategio.fa8bebd9.svg";export{s as _};
