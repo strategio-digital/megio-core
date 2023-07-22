0eef5afa7b343ff9b1ff36599d985e743ed1d3a6
Jiří Zapletal <jz@strategio.dev>
2023-07-22 12:23:16 -0600

    Fixed sqlite permissions

--- sandbox/docker-entrypoint.sh
+++ sandbox/docker-entrypoint.sh
@@ -10,6 +10,7 @@ php bin/console app:auth:resources:update
 # override permissions
 chown -R www-data:www-data /var/www/html/temp
 chown -R www-data:www-data /var/www/html/log
+chown -R www-data:www-data /var/www/html/docker/temp
 
 # start php-fpm and nginx
 php-fpm -D && nginx -g 'daemon off;'
