b56ab5e7ed273b67c10d754ffdfe243972d64878
Jiří Zapletal <jz@strategio.dev>
2023-07-24 12:46:37 -0600

    Add saas_versions in API

--- src/Http/Controller/AppController.php
+++ src/Http/Controller/AppController.php
@@ -8,10 +8,12 @@ declare(strict_types=1);
 namespace Saas\Http\Controller;
 
 use Nette\DI\Container;
+use Nette\Utils\Strings;
 use Saas\Helper\Path;
 use Saas\Helper\Router;
 use Saas\Http\Controller\Base\Controller;
 use Saas\Storage\Storage;
+use Siketyan\YarnLock\YarnLock;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 
@@ -28,6 +30,7 @@ class AppController extends Controller
         
         return $this->render(Path::saasVendorDir() . '/view/controller/admin.latte', [
             'appPath' => $appPath,
+            'saasVersions' => $this->versions(),
         ]);
     }
     
@@ -56,15 +59,54 @@ class AppController extends Controller
             'mode' => $_ENV['APP_ENV_MODE'],
             'log_adapter' => $_ENV['LOG_ADAPTER'],
             'storage_adapter' => $storage->getAdapterName(),
-            'execution_time' => floor((microtime(true) - $container->parameters['startedAt']) * 1000) . 'ms',
+            'saas_versions' => $this->versions(),
             'current_dt' => [
                 'date_time' => $dt->format('Y.m.d H:i:s:u'),
                 'time_zone' => $dt->getTimezone()->getName()
             ],
+            'execution_time' => floor((microtime(true) - $container->parameters['startedAt']) * 1000) . 'ms',
             'endpoints' => [
                 'count' => $routes->count(),
                 'items' => $prettyRoutes
             ]
         ]);
+    }
+    
+    /**
+     * @return array{ composer: string|null, yarn: string|null }
+     */
+    private function versions(): array
+    {
+        $composerVersion = null;
+        $yarnVersion = null;
+        $commit = null;
+        
+        $content = file_get_contents(Path::appDir() . '/../composer.lock');
+        
+        if ($content && $json = json_decode($content, true)) {
+            $composer = current(array_filter($json['packages'], fn($package) => $package['name'] === 'strategio/saas'));
+            
+            if ($composer) {
+                $composerVersion = $composer['version'];
+                $commit = $composer['source']['reference'];
+            }
+        }
+        
+        $content = file_get_contents(Path::appDir() . '/../yarn.lock');
+        
+        if ($content) {
+            $json = YarnLock::toArray($content);
+            $yarn = current(array_filter($json, fn($key) => Strings::startsWith($key, 'strategio-saas@'), ARRAY_FILTER_USE_KEY));
+            
+            if ($yarn) {
+                $yarnVersion = $yarn['version'];
+            }
+        }
+        
+        return [
+            'yarn' => $yarnVersion,
+            'composer' => $composerVersion,
+            'commit_reference' => $commit
+        ];
     }
 }
