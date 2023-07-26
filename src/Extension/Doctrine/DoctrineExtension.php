91f7a26f1b9dc9877936dc1b9ca6f1f0f33fb087
Jiří Zapletal <jz@strategio.dev>
2023-07-26 12:20:41 -0600

    Removed old namespace

--- src/Extension/Doctrine/DoctrineExtension.php
+++ src/Extension/Doctrine/DoctrineExtension.php
@@ -10,7 +10,6 @@ namespace Saas\Extension\Doctrine;
 use Doctrine\DBAL\Logging\DebugStack;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 use Nette\DI\CompilerExtension;
-use Saas\Extension\Doctrine\Debugger\SnapshotLogger;
 
 class DoctrineExtension extends CompilerExtension
 {
