e7997b18076bfc0005c383d8bc1ae605a47cf761
Jiří Zapletal <jz@strategio.dev>
2023-09-21 19:09:14 -0600

    Removed sandbox from vendor-repository

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-VERSION='master'
+# Repository info
+VERSION='0.0.1' #can be 'master', 'develop', whatever
+REPOSITORY_OWNER='strategio-digital'
+REPOSITORY_NAME='saas-startup'
+
+# Github
+GITHUB_FOLDER_NAME="${REPOSITORY_NAME}-${VERSION}"
+GITHUB_ZIP_URL="https://github.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/archive/refs/tags/${VERSION}.zip"
 
 # Colors
 RED='\033[0;31m'
@@ -17,37 +24,37 @@ help() {
 
 if test "$1" = "create"; then
   echo -e ""
-  echo -e "${GREEN}Project creation started...${NC}"
+    echo -e "${GREEN}Project creation started...${NC}"
 
-  mkdir $2
-  curl -L0 https://github.com/strategio-digital/saas/archive/refs/heads/${VERSION}.zip --output "./$2/project.zip"
-  unzip -q "./$2/project.zip" -d "./$2/"
-  cp -r "./$2/saas-${VERSION}/sandbox/" "./$2/"
-  rm -rf "./$2/saas-${VERSION}" "./$2/project.zip"
+    mkdir $2
+    curl -L0 ${GITHUB_ZIP_URL} --output "./$2/project.zip"
+    unzip -q "./$2/project.zip" -d "./$2/"
+    cp -r "./$2/${GITHUB_FOLDER_NAME}/" "./$2/"
+    rm -rf "./$2/${GITHUB_FOLDER_NAME}" "./$2/project.zip"
 
-  echo -e ""
-  echo -e "${GREEN}Project successfully created!"
-  echo -e "Now, you can follow installation guide in ${YELLOW}readme.md${NC}"
+    echo -e ""
+    echo -e "${GREEN}Project successfully created!"
+    echo -e "Now, you can follow installation guide in ${YELLOW}readme.md${NC}"
 
 elif test "$1" = "update"; then
   echo -e ""
   echo -e "${GREEN}Project update started...${NC}"
 
-  curl -L0 https://github.com/strategio-digital/saas/archive/refs/heads/${VERSION}.zip --output "./project.zip"
-  unzip -q "./project.zip" -d "./.strategio-saas/"
+  curl -L0 ${GITHUB_ZIP_URL} --output "./project.zip"
+  unzip -q "./project.zip" -d "./.temp-folder/"
 
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/bin/console" "./bin/console"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/docker/" "./docker/"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/www/index.php" "./www/index.php"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/.env.example" "./.env.example"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/.gitignore" "./.gitignore"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/docker-compose.yml" "./docker-compose.yml"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/docker-entrypoint.sh" "./docker-entrypoint.sh"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/Dockerfile" "./Dockerfile"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/project.sh" "./project.sh"
-  cp -r "./.strategio-saas/saas-${VERSION}/sandbox/vite.config.ts" "./vite.config.ts"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/bin/console" "./bin/console"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/docker/" "./docker/"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/www/index.php" "./www/index.php"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/.env.example" "./.env.example"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/.gitignore" "./.gitignore"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/docker-compose.yml" "./docker-compose.yml"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/docker-entrypoint.sh" "./docker-entrypoint.sh"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/Dockerfile" "./Dockerfile"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/project.sh" "./project.sh"
+  cp -r "./.temp-folder/${GITHUB_FOLDER_NAME}/vite.config.ts" "./vite.config.ts"
 
-  rm -rf "./project.zip" "./.strategio-saas" "./temp/*"
+  rm -rf "./project.zip" "./.temp-folder" "./temp/*", "./www/temp"
   echo -e ""
   echo -e "${GREEN}Project successfully updated!"
   echo -e "Now, you can run command ${YELLOW}'docker-compose up --build -d'${NC}."
--- sandbox/readme.md
+++
@@ -1,5 +0,0 @@
-# App name
-Application built on [Strategio SaaS](https://github.com/strategio-digital/saas).  
-
-## Installation
-https://docs.saas.strategio.dev/docs/getting-started/installation
