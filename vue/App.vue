1582beadec4160b56824c772eaedc30397fb99b5
Jiří Zapletal <jz@strategio.dev>
2023-10-06 16:23:43 -0600

    Removed vue-js from vendor

--- vue/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { VSonner } from 'vuetify-sonner'
-</script>
-
-<template>
-    <VSonner position="bottom-right" :visible-toasts="5" />
-    <router-view />
-</template>
--- vue/components/navbar/types/Constants.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright (c) 2023 Strategio Digital s.r.o.
- * @author Jiří Zapletal (https://strategio.dev, jz@strategio.dev)
- */
-
-export const COLLECTION_EMPTY_ROUTE = '@'
