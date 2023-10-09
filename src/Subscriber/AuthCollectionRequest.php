04bb9fad497738a204f0fd164365997362891658
Jiří Zapletal <jz@strategio.dev>
2023-10-09 10:42:55 -0600

    Removed unused namespaces

--- src/Subscriber/AuthCollectionRequest.php
+++ src/Subscriber/AuthCollectionRequest.php
@@ -14,7 +14,6 @@ use Megio\Security\Auth\AuthUser;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouteCollection;
 
 class AuthCollectionRequest implements EventSubscriberInterface
--- src/Subscriber/AuthRouteRequest.php
+++ src/Subscriber/AuthRouteRequest.php
@@ -12,7 +12,6 @@ use Megio\Security\Auth\AuthUser;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\RouteCollection;
