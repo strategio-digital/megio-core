5fb5c0e639f5873e9039f58d5fbc1f9e809e8f53
Jiří Zapletal <jz@strategio.dev>
2024-03-18 18:16:47 -0600

    Added UniqueRule

---
+++ src/Collection/Builder/Rule/UniqueRule.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Megio\Collection\Builder\Rule;
+
+use Doctrine\ORM\AbstractQuery;
+use Megio\Collection\Builder\BuilderEventName;
+use Megio\Collection\Builder\Rule\Base\BaseRule;
+
+class UniqueRule extends BaseRule
+{
+    /**
+     * @param string $columnName
+     * @param class-string $entityClassName
+     * @param string|null $message
+     */
+    public function __construct(
+        protected string      $entityClassName,
+        protected string      $columnName,
+        protected string|null $message = null
+    )
+    {
+        parent::__construct($message);
+    }
+    
+    public function name(): string
+    {
+        return 'unique';
+    }
+    
+    public function message(): string
+    {
+        return $this->message ?: "Value of '{$this->getField()->getName()}' must be unique";
+    }
+    
+    /**
+     * Return true if validation is passed
+     * @return bool
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function validate(): bool
+    {
+        $repo = $this->getEntityManager()->getRepository($this->entityClassName);
+        
+        $row = $repo->createQueryBuilder('e')
+            ->select('e')
+            ->where("e.{$this->columnName} = :value")
+            ->setParameter('value', $this->getValue())
+            ->getQuery()
+            ->getOneOrNullResult(AbstractQuery::HYDRATE_ARRAY)
+        ;
+        
+        if ($row === null) {
+            return true;
+        }
+        
+        $updating = $this->getBuilder()->getEventName() === BuilderEventName::UPDATE;
+        
+        if ($row && $updating && $row[$this->columnName] === $this->getValue()) {
+            return true;
+        }
+        
+        return false;
+    }
+}
