10094be36cb51377d6f27fb4de535cbb5833c820
Jiří Zapletal <jz@strategio.dev>
2024-11-09 19:18:41 +0100

    Add enum type column

---
+++ src/Collection/ReadBuilder/Column/EnumColumn.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Megio\Collection\ReadBuilder\Column;
+
+use Megio\Collection\Formatter\CallableFormatter;
+use Megio\Collection\ReadBuilder\Column\Base\BaseColumn;
+
+class EnumColumn extends BaseColumn
+{
+    public function renderer(): string
+    {
+        return 'string-column-renderer';
+    }
+    
+    public function __construct(
+        protected string $key,
+        protected string $name,
+        protected bool $sortable = false,
+        protected bool $visible = true,
+    )
+    {
+        parent::__construct(
+            key: $key,
+            name: $name,
+            sortable: $sortable,
+            visible: $visible,
+            formatters: [new CallableFormatter(fn($value) => $value ? $value->value : null)]
+        );
+    }
+}
