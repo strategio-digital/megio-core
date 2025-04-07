4e3564bdb1669366a8f5b57022645ede0ef44e76
Jiří Zapletal <jz@strategio.dev>
2025-04-07 18:47:50 +0200

    Fix linkResolver in CLI mode

--- src/Http/Resolver/LinkResolver.php
+++ src/Http/Resolver/LinkResolver.php
@@ -8,18 +8,34 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LinkResolver
 {
-    public function __construct(protected UrlGenerator $urlGenerator)
-    {
-    }
-    
+    public function __construct(protected UrlGenerator $urlGenerator) {}
+
     /**
      * @param string $name
      * @param array<string, string|int> $params
      * @param int $path
      * @return string
      */
-    public function link(string $name, array $params = [], int $path = UrlGeneratorInterface::ABSOLUTE_PATH): string
-    {
-        return $this->urlGenerator->generate($name, $params, $path);
+    public function link(
+        string $name,
+        array $params = [],
+        int $path = UrlGeneratorInterface::ABSOLUTE_PATH,
+    ): string {
+        $link = $this->urlGenerator->generate(
+            name: $name,
+            parameters: $params,
+            referenceType: $path,
+        );
+
+        if (php_sapi_name() == 'cli' && $path === UrlGeneratorInterface::ABSOLUTE_URL) {
+            $appUrl = $_ENV['APP_URL'];
+            if (str_ends_with($_ENV['APP_URL'], '/')) {
+                $appUrl = substr($_ENV['APP_URL'], 0, -1);
+            }
+
+            return $appUrl . $link;
+        }
+
+        return $link;
     }
 }
